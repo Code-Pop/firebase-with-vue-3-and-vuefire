359e51b222c100d1db7b98b8d9315e00c380e98c
Ben Hong <ben@bencodezen.io>
2023-02-21 08:25:52 -0800

    feature: user can sign in and out of the app

--- src/components/NavItems.vue
+++ src/components/NavItems.vue
@@ -1,12 +1,32 @@
 <script setup>
+import { useCurrentUser, useFirebaseAuth } from 'vuefire'
+import { signOut } from '@firebase/auth'
 import BaseButton from './base/BaseButton.vue'
+
+const user = useCurrentUser()
+console.log(user)
+
+const auth = useFirebaseAuth()
+
+async function signOutOfFirebase() {
+  signOut(auth)
+    .then(() => {
+      console.log('Logged out!')
+    })
+    .catch((error) => {
+      console.error(error)
+    })
+}
 </script>
 
 <template>
   <nav class="pr-4">
     <BaseButton to="/">Home</BaseButton>
     <BaseButton to="/new">New</BaseButton>
-    <BaseButton to="/sign-in">Sign-In</BaseButton>
+    <BaseButton v-if="user?.email" @click="signOutOfFirebase"
+      >Sign Out</BaseButton
+    >
+    <BaseButton v-else to="/sign-in">Sign-In</BaseButton>
   </nav>
 </template>
 
--- src/views/SignInPage.vue
+++ src/views/SignInPage.vue
@@ -1,7 +1,10 @@
 <script setup>
 import { ref } from 'vue'
 import { useFirebaseAuth } from 'vuefire'
-import { createUserWithEmailAndPassword } from '@firebase/auth'
+import {
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+} from '@firebase/auth'
 
 import BaseButton from '@/components/base/BaseButton.vue'
 import BaseContainer from '@/components/base/BaseContainer.vue'
@@ -9,7 +12,7 @@ import BaseCard from '@/components/base/BaseCard.vue'
 import BaseForm from '@/components/base/BaseForm.vue'
 import BaseInput from '@/components/base/BaseInput.vue'
 
-const newUser = ref({
+const userInput = ref({
   email: '',
   password: '',
 })
@@ -19,8 +22,8 @@ const auth = useFirebaseAuth()
 async function createUser() {
   createUserWithEmailAndPassword(
     auth,
-    newUser.value.email,
-    newUser.value.password
+    userInput.value.email,
+    userInput.value.password
   )
     .then((userCredential) => {
       // Signed in
@@ -34,6 +37,23 @@ async function createUser() {
       // ..
     })
 }
+
+async function signInToFirebase() {
+  signInWithEmailAndPassword(
+    auth,
+    userInput.value.email,
+    userInput.value.password
+  )
+    .then((userCredential) => {
+      // Signed in
+      const user = userCredential.user
+      // ...
+    })
+    .catch((error) => {
+      const errorCode = error.code
+      const errorMessage = error.message
+    })
+}
 </script>
 
 <template>
@@ -43,14 +63,14 @@ async function createUser() {
       <template v-slot:default>
         <BaseForm>
           <BaseInput
-            v-model="newUser.email"
+            v-model="userInput.email"
             type="email"
             label="Email"
             required
             placeholder="eleanorshellstrop@thegoodplace.com"
           />
           <BaseInput
-            v-model="newUser.password"
+            v-model="userInput.password"
             label="Password"
             type="password"
             required
@@ -58,7 +78,9 @@ async function createUser() {
         </BaseForm>
       </template>
       <template v-slot:actions>
-        <BaseButton variant="tonal" color="success"> Sign In </BaseButton>
+        <BaseButton @click="signInToFirebase" variant="tonal" color="success">
+          Sign In
+        </BaseButton>
         <BaseButton
           @click="createUser"
           variant="tonal"
